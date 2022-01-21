8ac61df814eabcfa804880920bfefb360facfcdc
ffzanini <devffzanini@gmail.com>
2022-01-21 20:16:20 -0300

    added quiz game files

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Questions } from './constants/Questions';
+
+export default function App() {
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < Questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					Tu pontuou {score} de {Questions.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Questão: {currentQuestion + 1}</span>/{Questions.length}
+						</div>
+						<div className='question-text'>{Questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{Questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
+}
---
+++ src/constants/Questions.js
@@ -0,0 +1,119 @@
+export const Questions = [
+{
+  questionText: 'Qual minha cor favorita?',
+  answerOptions: [
+    { answerText: 'Vermelho', isCorrect: false },
+    { answerText: 'Preto', isCorrect: false },
+    { answerText: 'Verde', isCorrect: true },
+    { answerText: 'Amarelo', isCorrect: false },
+  ],
+},
+{
+  questionText: 'Qual superpoder eu gostaria de ter?',
+  answerOptions: [
+    { answerText: 'Super Força', isCorrect: false },
+    { answerText: 'Voar', isCorrect: true },
+    { answerText: 'Super Velocidade', isCorrect: false },
+    { answerText: 'Olhos lazer', isCorrect: false },
+  ],
+},
+{
+  questionText: 'Qual meu personagem favorito de séries?',
+  answerOptions: [
+    { answerText: 'Ragnar Lothbrok', isCorrect: false },
+    { answerText: 'Thomas Shelby', isCorrect: false },
+    { answerText: 'Geralt de Rívia', isCorrect: false },
+    { answerText: 'Todos as alternativas acima', isCorrect: true },
+  ],
+},
+{
+  questionText: 'Qual meu personagem favorito de séries animadas?',
+  answerOptions: [
+    { answerText: 'Akagi', isCorrect: false },
+    { answerText: 'L', isCorrect: false },
+    { answerText: 'Rick', isCorrect: false },
+    { answerText: 'Todas as alternativas acima', isCorrect: true },
+  ],
+},
+{
+  questionText: 'Qual meu personagem favorito da Marvel?',
+  answerOptions: [
+    { answerText: 'Hulk', isCorrect: false },
+    { answerText: 'Wolverine', isCorrect: true },
+    { answerText: 'Homem de Ferro', isCorrect: false },
+    { answerText: 'Thor', isCorrect: false },
+  ],
+},
+{
+  questionText: 'Qual meu personagem favorito da DC?',
+  answerOptions: [
+    { answerText: 'Caçador de Marte', isCorrect: false },
+    { answerText: 'Super Homem', isCorrect: false },
+    { answerText: 'Batman', isCorrect: true },
+    { answerText: 'Lanterna Verde', isCorrect: false },
+  ],
+},
+{
+  questionText: 'Qual meu filme favorito?',
+  answerOptions: [
+    { answerText: 'Star Wars', isCorrect: false },
+    { answerText: 'Senhor dos Anéis', isCorrect: false },
+    { answerText: 'Samurai X', isCorrect: false },
+    { answerText: 'Todas as alternativas acima', isCorrect: true },
+  ],
+},
+{
+  questionText: 'Qual minha série favorita?',
+  answerOptions: [
+    { answerText: 'Breaking Bad', isCorrect: false },
+    { answerText: 'The Witcher', isCorrect: false },
+    { answerText: 'Peaky Blinders', isCorrect: false },
+    { answerText: 'Todas as alternativas acima', isCorrect: true },
+  ],
+},
+{
+  questionText: 'Qual meu livro favorito?',
+  answerOptions: [
+    { answerText: 'Guia do Mochileiro das Galáxias', isCorrect: true },
+    { answerText: 'Como vencer quando você não é o favorito', isCorrect: false },
+    { answerText: 'Uma breve história do tempo', isCorrect: false },
+    { answerText: 'The Witcher: A espada do destino', isCorrect: false },
+  ],
+},
+{
+  questionText: 'Qual minha estação do ano favorita?',
+  answerOptions: [
+    { answerText: 'Inverno', isCorrect: true },
+    { answerText: 'Verão', isCorrect: false },
+    { answerText: 'Outono', isCorrect: false },
+    { answerText: 'Primavera', isCorrect: false },
+  ],
+},
+{
+  questionText: 'Pra onde eu mais quero ir viajar?',
+  answerOptions: [
+    { answerText: 'Canadá', isCorrect: false },
+    { answerText: 'Itália', isCorrect: false },
+    { answerText: 'Nova Zelândia', isCorrect: true },
+    { answerText: 'Austrália', isCorrect: false },
+  ],
+},
+{
+  questionText: 'Quantos irmãos eu tenho?',
+  answerOptions: [
+    { answerText: '3', isCorrect: false },
+    { answerText: '1', isCorrect: false },
+    { answerText: '2', isCorrect: true },
+    { answerText: '4', isCorrect: false },
+  ],
+},
+{
+  questionText: 'Qual era a sua profissão dos sonhos na infância?',
+  answerOptions: [
+    { answerText: 'Bombeiro', isCorrect: false },
+    { answerText: 'Policial', isCorrect: true },
+    { answerText: 'Astronauta', isCorrect: false },
+    { answerText: 'Militar', isCorrect: false },
+  ],
+},
+];
